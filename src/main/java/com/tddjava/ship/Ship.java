ad273858921881a6611dd13b6d6ca1d704d1eb5a
binbjz <8213913+binbjz@users.noreply.github.com>
2024-01-14 16:24:23 +0800

    Add ship implementation

--- src/main/java/com/tddjava/ship/Ship.java
+++ src/main/java/com/tddjava/ship/Ship.java
@@ -1,5 +1,35 @@
 package com.tddjava.ship;
 
 public class Ship {
+    private Location location;
+    private Planet planet;
 
+    public Ship(Location location, Planet planet) {
+        this.location = location;
+        this.planet = planet;
+    }
+
+    public boolean moveForward() {
+        return location.forward(planet.getMax(), planet.getObstacles());
+    }
+
+    public boolean moveBackward() {
+        return location.backward(planet.getMax(), planet.getObstacles());
+    }
+
+    public void turnLeft() {
+        location.turnLeft();
+    }
+
+    public void turnRight() {
+        location.turnRight();
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public Planet getPlanet() {
+        return planet;
+    }
 }
